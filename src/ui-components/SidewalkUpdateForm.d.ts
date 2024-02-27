ad513bcdaaef53be6405456e44ae78fe35f77416
LudiJolo <joloh18@yahoo.com>
2024-02-27 12:08:34 -0800

    database and schema setup

---
+++ src/ui-components/SidewalkUpdateForm.d.ts
@@ -0,0 +1,67 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+import * as React from "react";
+import { GridProps, TextFieldProps } from "@aws-amplify/ui-react";
+export declare type EscapeHatchProps = {
+    [elementHierarchy: string]: Record<string, unknown>;
+} | null;
+export declare type VariantValues = {
+    [key: string]: string;
+};
+export declare type Variant = {
+    variantValues: VariantValues;
+    overrides: EscapeHatchProps;
+};
+export declare type ValidationResponse = {
+    hasError: boolean;
+    errorMessage?: string;
+};
+export declare type ValidationFunction<T> = (value: T, validationResponse: ValidationResponse) => ValidationResponse | Promise<ValidationResponse>;
+export declare type SidewalkUpdateFormInputValues = {
+    SID?: number;
+    x_slope?: number;
+    y_slope?: number;
+    h_displacement?: number;
+    v_displacement?: number;
+    compliance?: string;
+    lat?: number;
+    lon?: number;
+};
+export declare type SidewalkUpdateFormValidationValues = {
+    SID?: ValidationFunction<number>;
+    x_slope?: ValidationFunction<number>;
+    y_slope?: ValidationFunction<number>;
+    h_displacement?: ValidationFunction<number>;
+    v_displacement?: ValidationFunction<number>;
+    compliance?: ValidationFunction<string>;
+    lat?: ValidationFunction<number>;
+    lon?: ValidationFunction<number>;
+};
+export declare type PrimitiveOverrideProps<T> = Partial<T> & React.DOMAttributes<HTMLDivElement>;
+export declare type SidewalkUpdateFormOverridesProps = {
+    SidewalkUpdateFormGrid?: PrimitiveOverrideProps<GridProps>;
+    SID?: PrimitiveOverrideProps<TextFieldProps>;
+    x_slope?: PrimitiveOverrideProps<TextFieldProps>;
+    y_slope?: PrimitiveOverrideProps<TextFieldProps>;
+    h_displacement?: PrimitiveOverrideProps<TextFieldProps>;
+    v_displacement?: PrimitiveOverrideProps<TextFieldProps>;
+    compliance?: PrimitiveOverrideProps<TextFieldProps>;
+    lat?: PrimitiveOverrideProps<TextFieldProps>;
+    lon?: PrimitiveOverrideProps<TextFieldProps>;
+} & EscapeHatchProps;
+export declare type SidewalkUpdateFormProps = React.PropsWithChildren<{
+    overrides?: SidewalkUpdateFormOverridesProps | undefined | null;
+} & {
+    id?: string;
+    sidewalk?: any;
+    onSubmit?: (fields: SidewalkUpdateFormInputValues) => SidewalkUpdateFormInputValues;
+    onSuccess?: (fields: SidewalkUpdateFormInputValues) => void;
+    onError?: (fields: SidewalkUpdateFormInputValues, errorMessage: string) => void;
+    onChange?: (fields: SidewalkUpdateFormInputValues) => SidewalkUpdateFormInputValues;
+    onValidate?: SidewalkUpdateFormValidationValues;
+} & React.CSSProperties>;
+export default function SidewalkUpdateForm(props: SidewalkUpdateFormProps): React.ReactElement;
